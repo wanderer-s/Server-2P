7f658dde76c826c0902a14bf99544c78e22840cc
wonjun0120 <wonjundero@naver.com>
2020-07-18 16:53:02 +0900

    "controllers/user/signup.js, routes/user.js"_Fix remark and module path where merge models

--- web_server/controllers/user/signup.js
+++ web_server/controllers/user/signup.js
@@ -1,16 +1,14 @@
-var models = require('../models');
+var models = require('../../models/users');
 
 module.exports = {
-	signup: {
-		post: async function (req, res) {
-			try {
-				let params = [req.body.userid, req.body.password, req.body.nickname];
-				await models.users.post(params);
-				res.status(200).json({'message' : '회원가입 성공했습니다.'});
-			} catch (err) {
-				// console.error(err);
-				res.status(409).json({'error' : '이미 존재하는 아이디/닉네임 입니다.'});
-			}
+	post: async function (req, res) {
+		try {
+			let params = [req.body.userid, req.body.password, req.body.nickname];
+			await models.signup.post(params);
+			res.status(200).json({ 'message': '회원가입 성공했습니다.' });
+		} catch (err) {
+			console.error(err);
+			res.status(409).json({'error': JSON.stringify(err)});
 		}
 	}
 };
--- web_server/routes/user.js
+++ web_server/routes/user.js
@@ -3,16 +3,16 @@ const router = express.Router();
 
 const { userController } = require('../controllers');
 
-// * POST /user/signin
-router.post('/signin', userController.signin.post);
+// // * POST /user/signin
+// router.post('/signin', userController.signin.post);
 
-// * POST /user/signout
-router.post('/signout', userController.signout.post);
+// // * POST /user/signout
+// router.post('/signout', userController.signout.post);
 
 // * POST /user/signup
 router.post('/signup', userController.signup.post);
 
-// * GET /user/info
-router.get('/info', userController.info.get);
+// // * GET /user/info
+// router.get('/info', userController.info.get);
 
 module.exports = router;
