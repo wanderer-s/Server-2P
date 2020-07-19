90eab14d0b0d88219af066a9d3a24dba5da3cee5
wonjun0120 <wonjundero@naver.com>
2020-07-19 14:42:16 +0900

    "controllers/rooms/index.js"_Fix function name

--- web_server/controllers/rooms/index.js
+++ web_server/controllers/rooms/index.js
@@ -11,27 +11,29 @@ let moleRooms = {};
 }
 */
 module.exports = {
-	post: async function (req, res) {
-		try {
-			let params = {
-				roomId: momnet().format('YYYYMMDDHHMMSS') + req.body.roomName,
-				gameCode: req.body.gameCode,
-				password: req.body.password,
-				roomName: req.body.roomName,
-				roomOwner: req.body.roomOwner
-			};
-			
-			if(params.gameCode === 1){	//두더지 게임
-				//Owner확인, 이미 생성된 방중 Owner가 겹치면 기존에 있던 방 삭제..?
-				moleRooms[params.roomId] = params;
+	makeroom: {
+		post: async function (req, res) {
+			try {
+				let params = {
+					roomId: momnet().format('YYYYMMDDHHMMSS') + req.body.roomName,
+					gameCode: req.body.gameCode,
+					password: req.body.password,
+					roomName: req.body.roomName,
+					roomOwner: req.body.roomOwner
+				};
+				
+				if(params.gameCode === 1){	//두더지 게임
+					//Owner확인, 이미 생성된 방중 Owner가 겹치면 기존에 있던 방 삭제..?
+					moleRooms[params.roomId] = params;
+				}
+	
+				res.status(200).json({ 'message': '방을 성공적으로 생성했습니다.' });
+				// console.log(moleRooms);
+				//redirection?
+			} catch (err) {
+				console.error(err);
+				res.status(501).json({ 'error': JSON.stringify(err) });
 			}
-
-			res.status(200).json({ 'message': '방을 성공적으로 생성했습니다.' });
-			// console.log(moleRooms);
-			//redirection?
-		} catch (err) {
-			console.error(err);
-			res.status(501).json({ 'error': JSON.stringify(err) });
 		}
 	}
 };
