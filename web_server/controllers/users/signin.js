bbc540b51fc13fb8507a77d4359e4d7c49efe708
wonjun0120 <wonjundero@naver.com>
2020-07-20 20:37:48 +0900

    "controllers"_Add signin API

---
+++ web_server/controllers/users/signin.js
@@ -0,0 +1,27 @@
+var models = require('../../models/users');
+
+module.exports = {
+	post: async function (req, res) {
+		try {
+			let sess = req.session;
+
+			let params = {
+				userId: req.body.userId,
+				password: req.body.password,
+				socialId: req.body.socialId,
+			};
+
+			let data = await models.signin.post(params);
+			// console.log(data[0].id);
+
+			//session
+			sess.userId = data[0].id;
+			// console.log(sess);
+			res.status(200).json({ 'message': '로그인 성공하였습니다.' });
+		} catch (err) {
+			console.error(err);
+			if(err.message === 'loginErr') res.status(403).json({'error' : '로그인 실패하였습니다.'});
+			else res.status(501).json(err);
+		}
+	}
+};
