1b800fe0204af380640d0f22496aed2405739fc4
wonjun0120 <wonjundero@naver.com>
2020-07-21 11:51:32 +0900

    "web_server"_Add signout API

--- web_server/app.js
+++ web_server/app.js
@@ -17,7 +17,15 @@ const port = 3001;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors());
+// app.use(cors());
+
+app.use(
+	cors({
+		origin: ['http://localhost:3000'],
+		methods: ['GET', 'POST', 'DELETE'],
+		credentials: true
+	})
+);
 
 app.use(
 	session({
