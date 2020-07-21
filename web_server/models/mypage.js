b3f2c4f8f83c9bef597aa0b3e260a1b9b47bbd8e
Wanderer-s <ows0871@gmail.com>
2020-07-22 00:22:13 +0900

    models/mypage.js_Add mypage of models

--- web_server/models/mypage.js
+++ web_server/models/mypage.js
@@ -6,31 +6,44 @@ module.exports = {
 		return new Promise((resolve, reject) => {
 			
 			let sql=`
-				select 
+				select
 					gl.gameName,
-					ug.gamesPlayed,
-					ug.gamesWon,
-					ug.gamesTied,
-					ug.gamesPlayed - gamesWon - gamesTied as gameLose
+					p.gamesPlayed,
+					p.gamesWon,
+					p.gamesTied,
+					p.gamesPlayed - p.gamesWon - p.gamesTied as gamesLost
 				from users_game ug
 				join gameList gl on gl.code = ug.gameCode
+				join playerscore p on p.scoreId = ug.id
 				where ug.userId = ?
 				order by gl.gameName
 				`;
-			db.query(sql, data, (error, result) => {
-				if(error) {
-					reject(error);
+				
+			db.query(sql, data, (error1, result1) => {
+				if(error1) {
+					reject(error1);
 				} else {
-					console.log(JSON.stringify(result));
 					let sql =`
 							select nickname, avatarId
 							from users
 							where id = ?`;
-					db.query(sql, data, (error, result) => {
-						if(error) {
-							reject(error);
+					db.query(sql, data, (error2, result2) => {
+						if(error2) {
+							reject(error2);
 						} else {
-							console.log(result);
+							if(result1.length === 0) {
+								resolve(result2);
+							} else {
+								for(let row of result1) {
+									result2[0][row.gameName] = {
+										play : row.gamesPlayed,
+										win : row.gamesWon,
+										tie : row.gamesTied,
+										lose : row.gamesLost
+									};
+								}
+								resolve(result2);
+							}
 						}
 					});
 				}
@@ -38,15 +51,15 @@ module.exports = {
 		});
 	},
 	put: async function(data) {
-		let {sessionKey, avatarId} = data;
-
+		let {userId, avatarId} = await data;
+		
 		return new Promise((resolve, reject) => {
 			let sql = `
 					update users
 					set avatarId = ?
 					where id = ?
 					`;
-			db.query(sql, [avatarId, sessionKey], (error, result) => {
+			db.query(sql, [avatarId, userId], (error, result) => {
 				error? reject(error) : resolve(result);
 			});
 		});
