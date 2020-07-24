9d680a80e28e77f16a8328877e868e7c1b657193
wonjun0120 <wonjundero@naver.com>
2020-07-24 15:02:12 +0900

    "chat_server/index.js"_Fix chat socket server

--- chat_server/index.js
+++ chat_server/index.js
@@ -6,10 +6,8 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
-
 /*
   user: {
-    userId: 
     username: //..?nickname
     avatar:
     isReady:
@@ -17,8 +15,6 @@ const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/u
   room: {
     gameCode:
     roomId:
-    roomName:
-    roomOwner:
   }
 */
 
@@ -28,19 +24,21 @@ io.on('connection', (socket) => {
   socket.on('joinRoom', ({ userInfo, room }) => {
     const user = userJoin(socket.id, userInfo, room);
     socket.join(user.room.roomId);
-    socket.emit('message', { username: 'System', text: '대기방에 입장하셨습니다!' });
+    socket.emit('systemMessage', { username: 'System', text: '대기방에 입장하셨습니다!' });
+    // socket.emit('loadUser', { username, avatar, isLogin: false });
+    io.to(user.room.roomId).emit('loadUser', { username, avatar, isLogin: false });
     socket.broadcast
       .to(user.room.roomId)
-      .emit('message', { username: 'System', text: `${user.userInfo.username} has joined the chat` });
+      .emit('sendMessage', { username: 'System', text: `${user.username} has joined the chat` });
     io.to(user.room.roomId).emit('roomUsers', {
       room: user.room.roomId,
       users: getRoomUsers(user.room.roomId)
     });
   });
   // 채팅
-  socket.on('message', (msg) => {
+  socket.on('sendMessage', (msg) => {
     const user = getCurrentUser(socket.id);
-    io.to(user.room.roomId).emit('message', { username: user.userInfo.username, text: msg });
+    io.to(user.room.roomId).emit('sendMessage', { username: user.userInfo.username, text: msg });
   });
   // 준비 버튼
   socket.on('ready', () => {
@@ -59,28 +57,33 @@ io.on('connection', (socket) => {
         });
     const roomUsers = getRoomUsers(user.room.roomId);
     //전부 준비한 상황
-    if (roomUsers[0].userInfo.isReady && roomUsers[1].userInfo.isReady) { 
+    if (roomUsers[0].userInfo.isReady && roomUsers[1].userInfo.isReady) {
       socket.broadcast
         .to(user.room.roomId)
         .emit('message', { username: 'System', text: 'gameStart!' });
     }
   });
+  socket.on('leave', () => {
+    const user = getCurrentUser(socket.id);
+    userLeave(socket.id);
+    socket.leave(user.room.roomId);
+    socket.broadcast
+      .to(user.room.roomId)
+      .emit('systemMessage', { username: 'System', text: `${user.userInfo.username} has left the chat` });
+  });
   // 방 나가기
   socket.on('disconnect', () => {
     const user = userLeave(socket.id);
     if (user) {
-      io.to(user.room.roomId).emit(
-        'message',
-        formatMessage(botName, `${user.userInfo.username} has left the chat`)
-      );
-
-      // Send users and room info
-      io.to(user.room.roomId).emit('roomUsers', {
-        room: user.room.roomId,
-        users: getRoomUsers(user.room.roomId),
+      io.to(user.room.roomId).emit('systemMessage', {
+        username: 'system',
+        text: `${user.userInfo.username} has left the room`,
       });
+
     }
   });
 });
-const PORT = process.env.PORT || 3001;
+
+
+const PORT = process.env.PORT || 3002;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
