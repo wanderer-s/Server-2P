8395bf3627a8d1b9f47bca35a390e390a8c683e5
Wanderer-s <ows0871@gmail.com>
2020-07-27 13:44:27 +0900

    mole_server basic setting

--- game_mole/server.js
+++ game_mole/server.js
@@ -3,11 +3,17 @@ const app = require('express')()
 const httpServer = createServer(app)
 const io = require('socket.io')(httpServer)
 
-const PORT = 3002
+const PORT = 3009
+
+app.get('/', (req, res) => {
+	res.status(200).send('<h1>welcome<h1/>')
+})
+
 io.on('connect', (socket) => {
 	console.log('new connection')
 	socket.emit('message', 'welcome')
 })
+
 httpServer.listen(PORT, () => {
 	console.log(`server on ${PORT}`)
 })
