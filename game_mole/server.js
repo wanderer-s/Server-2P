f33f0dcdf0e769bbbdcd2c509c86c4ff595dc390
Wanderer-s <ows0871@gmail.com>
2020-07-28 16:15:25 +0900

    game_mole/server.js Add socket io to receive and send data

--- game_mole/server.js
+++ game_mole/server.js
@@ -3,15 +3,32 @@ const app = require('express')()
 const httpServer = createServer(app)
 const io = require('socket.io')(httpServer)
 
+const {gameJoin, getCurrentScores,} = require('./utils')
+
 const PORT = 3009
 
-app.get('/', (req, res) => {
-	res.status(200).send('<h1>welcome<h1/>')
-})
-
 io.on('connect', (socket) => {
-	console.log('new connection')
-	socket.emit('message', 'welcome')
+	socket.on('gameStart', (username, gameRoomId) => {
+		let game = gameJoin(username, gameRoomId)
+		if(game.usernames.length === 2) {
+			const moleTimer = setInterval( () => {
+				let randomIndex = Math.floor(Math.random() * 16)
+				socket.emit('generateMole', randomIndex)
+			}, 3000)
+			setTimeout(() => {
+				clearInterval(moleTimer)
+				let scores = getCurrentScores(gameRoomId)
+				let player = Object.keys(scores)
+				let winner = 
+				scores[player[0]] > scores[player[1]]
+					? player[0]
+					: scores[player[0]] < scores[player[1]]
+						? player[1]
+						: 'tie'
+				socket.emit('gameover', winner)
+			},93000)
+		}
+	})
 })
 
 httpServer.listen(PORT, () => {
