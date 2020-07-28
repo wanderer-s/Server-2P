d74eb12160d971c360dc5867c70caa3576a9e59e
Phillip Choi <jaeja-phil@gmail.com>
2020-07-28 16:51:21 +0900

    merging upstream dev

--- game_mole/server.js
+++ game_mole/server.js
@@ -1,36 +1,38 @@
-const { createServer } = require('http')
-const app = require('express')()
-const httpServer = createServer(app)
-const io = require('socket.io')(httpServer)
+/* eslint-disable indent */
+const { createServer } = require('http');
+const app = require('express')();
+const httpServer = createServer(app);
+const io = require('socket.io')(httpServer);
 
-const {gameJoin, getCurrentScores,} = require('./utils')
+const { gameJoin, getCurrentScores } = require('./utils/users');
 
-const PORT = 3009
+const PORT = 3009;
 
 io.on('connect', (socket) => {
-	socket.on('gameStart', (username, gameRoomId) => {
-		let game = gameJoin(username, gameRoomId)
-		if(game.usernames.length === 2) {
-			const moleTimer = setInterval( () => {
-				let randomIndex = Math.floor(Math.random() * 16)
-				socket.emit('generateMole', randomIndex)
-			}, 3000)
-			setTimeout(() => {
-				clearInterval(moleTimer)
-				let scores = getCurrentScores(gameRoomId)
-				let player = Object.keys(scores)
-				let winner = 
-				scores[player[0]] > scores[player[1]]
-					? player[0]
-					: scores[player[0]] < scores[player[1]]
-						? player[1]
-						: 'tie'
-				socket.emit('gameover', winner)
-			},93000)
-		}
-	})
-})
+  socket.on('gameStart', (username, gameRoomId) => {
+    let game = gameJoin(username, gameRoomId);
+    socket.join(gameRoomId);
+    if (game.usernames.length === 2) {
+      const moleTimer = setInterval(() => {
+        let randomIndex = Math.floor(Math.random() * 16);
+        io.to(gameRoomId).emit('generateMole', randomIndex);
+      }, 3000);
+      setTimeout(() => {
+        clearInterval(moleTimer);
+        let scores = getCurrentScores(gameRoomId);
+        let player = Object.keys(scores);
+        let winner =
+          scores[player[0]] > scores[player[1]]
+            ? player[0]
+            : scores[player[0]] < scores[player[1]]
+            ? player[1]
+            : 'tie';
+        socket.emit('gameover', winner);
+      }, 93000);
+    }
+  });
+});
 
 httpServer.listen(PORT, () => {
-	console.log(`server on ${PORT}`)
-})
+  console.log(`server on ${PORT}`);
+});
--- game_mole/utils/users.js
+++ game_mole/utils/users.js
@@ -1,49 +1,49 @@
-
+/* eslint-disable no-unused-vars */
+/* eslint-disable indent */
 const games = [];
 
 // Join game
 function gameJoin(nickname, gameRoomId) {
-	let game = {
-		gameRoomId,
-		usernames : [],
-		currentMole: 0,
-		score: {
-		}
-	}
-	if(!games.find(gameRoomId => gameRoomId)) {
-		game.usernames.push(nickname)
-		game.score[nickname] = 0
-		games.push(game);
-		return game
-	} else {
-		games.find(gameRoomId => gameRoomId).usernames.push(nickname)
-		games.find(gameRoomId => gameRoomId).score[nickname] = 0
-		return games.find(gameRoomId => gameRoomId)
-	}
+  let game = {
+    gameRoomId,
+    usernames: [],
+    currentMole: 0,
+    score: {},
+  };
+  if (!games.find((gameRoomId) => gameRoomId)) {
+    game.usernames.push(nickname);
+    game.score[nickname] = 0;
+    games.push(game);
+    return game;
+  } else {
+    games.find((gameRoomId) => gameRoomId).usernames.push(nickname);
+    games.find((gameRoomId) => gameRoomId).score[nickname] = 0;
+    return games.find((gameRoomId) => gameRoomId);
+  }
 }
 
 // Get current users
 function getCurrentScores(gameRoomId) {
-	return getCurrentGame(gameRoomId).score
+  return getCurrentGame(gameRoomId).score;
 }
 
-
 function getCurrentGame(gameRoomId) {
-	return games.find(gameRoomId => gameRoomId)
+  return games.find((gameRoomId) => gameRoomId);
 }
 
 function leaveGame(gameRoomId) {
-	games.splice(games.findIndex(gameRoomId => gameRoomId), 1)
+  games.splice(
+    games.findIndex((gameRoomId) => gameRoomId),
+    1
+  );
 }
 
-
-exports.module = {
-	gameJoin,
-	getCurrentGame,
-	getCurrentScores,
-	leaveGame
-}
-
+module.exports = {
+  gameJoin,
+  getCurrentGame,
+  getCurrentScores,
+  leaveGame,
+};
 
 /* 
 function
