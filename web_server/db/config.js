20fc49c4c6929ef26c0ee47c96dfdf0e1658ce19
Wanderer-s <ows0871@gmail.com>
2020-08-03 04:03:10 +0900

    "config.js" fix mysql2 module to mysql2/promise and createConnection to createPool

--- web_server/db/config.js
+++ web_server/db/config.js
@@ -1,24 +1,24 @@
 /* eslint-disable */
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
-const development = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  // password: process.env.DATABASE_PRODUCTION_PASSWORD,
-  password: 'dnjswnsqkqh123',
-  database: '2p4p',
-});
-development.connect();
 
-/* const production = mysql.createConnection({
-	host: process.env.DATABASE_PRODUCTION_HOST,
-	user: process.env.DATABASE_PRODUCTION_USER,
-	password: process.env.DATABASE_PRODUCTION_PASSWORD,
-	database: '2p4p'
-})
-poduction.connect()
- */
+  const development = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DATABASE_DEV_PASSWORD,
+    database: '2p4p',
+  });
+  
+  /* const production = mysql.createConnection({
+    host: process.env.DATABASE_PRODUCTION_HOST,
+    user: process.env.DATABASE_PRODUCTION_USER,
+    password: process.env.DATABASE_PRODUCTION_PASSWORD,
+    database: '2p4p'
+  })
+  poduction.connect()
+   */
+  
+  module.exports = development;
 
-module.exports = development;
