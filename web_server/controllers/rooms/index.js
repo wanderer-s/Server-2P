e8c90ac099a0fef3fa612f5d67f4c636a9804481
Phillip Choi <jaeja-phil@gmail.com>
2020-08-05 00:17:22 +0900

    "web_server"Fix room apis

--- web_server/controllers/rooms/index.js
+++ web_server/controllers/rooms/index.js
@@ -1,8 +1,10 @@
-const momnet = require('moment');
+/* eslint-disable indent */
+const moment = require('moment');
 
 let moleRooms = {};
 let pongRooms = {};
 let cardRooms = {};
+
 /*
 {
 	"roomId":,
@@ -14,118 +16,141 @@ let cardRooms = {};
 }
 */
 module.exports = {
-	makeroom: {
-		post: async function (req, res) {
-			try {
-				let params = {
-					roomId: momnet().format('YYYYMMDDHHMMSS') + '-' +  req.body.roomName,
-					gameCode: req.body.gameCode,
-					password: req.body.password,
-					roomName: req.body.roomName,
-					roomOwner: req.session.username,  
-					userNum: 1
-				};
-				
-				if (params.gameCode === '1') {	//두더지 게임
-					moleRooms[params.roomId] = params;
-				} else if (params.gameCode === '2') {   //핑퐁게임
-					pongRooms[params.roomId] = params;
-				} else if (params.gameCode === '3') {   //추가게임
-					cardRooms[params.roomId] =params;
-				}
+  makeroom: {
+    post: async function (req, res) {
+      try {
+        let params = {
+          roomId: moment().format('YYYYMMDDHHMMSS') + '-' + req.body.roomName,
+          gameCode: req.body.gameCode,
+          password: req.body.password || '',
+          roomName: req.body.roomName,
+          roomOwner: req.body.username,
+          userNum: 1,
+        };
 
-				res.status(200).json({ 'message': '방을 성공적으로 생성했습니다.' });
-				// console.log(moleRooms);
-				//redirection?
-			} catch (err) {
-				console.error(err);
-				res.status(501).json({ 'error': JSON.stringify(err) });
-			}
-		}
-	},
-	roomlist: {
-		get: function (req, res) {
-			console.log('roomList', moleRooms);
-			try {
-				let gameCode = req.cookies.selectedGame;
-				let getRooms;
-				let rooms = [];
+        if (params.gameCode === '1') {
+          //두더지 게임
+          moleRooms[params.roomId] = params;
+        } else if (params.gameCode === '2') {
+          //핑퐁게임
+          pongRooms[params.roomId] = params;
+        } else if (params.gameCode === '3') {
+          //추가게임
+          cardRooms[params.roomId] = params;
+        }
 
-				if(gameCode === '1'){
-					getRooms = moleRooms;
-				} else if(gameCode === '2'){
-					getRooms = pongRooms;
-				} else if(gameCode === '3'){
-					getRooms = cardRooms;
-				}
+        res.status(200).json({ message: '방을 성공적으로 생성했습니다.' });
+        // console.log(moleRooms);
+        //redirection?
+      } catch (err) {
+        console.error(err);
+        res.status(501).json({ error: JSON.stringify(err) });
+      }
+    },
+  },
+  roomlist: {
+    get: function (req, res) {
+      try {
+        let gameCode = req.cookies.selectedGame;
+        let getRooms;
+        let rooms = [];
 
-				Object.keys(getRooms).forEach(k => {
-					let room = {};
-					room.roomId = getRooms[k].roomId;
-					room.gameCode = getRooms[k].gameCode;
-					room.roomName = getRooms[k].roomName;
-					room.roomOwner = getRooms[k].roomOwner;
-					room.userNum = getRooms[k].userNum;
-					room.isLock = Boolean(getRooms[k].password);
-					rooms.push(room);
-				});
-				console.log(rooms);
-				// res.status(200).json(moleRooms);  //변경 필요
-				res.status(200).json(rooms);  
-          
-			} catch (err) {
-				console.log(err);
-				res.status(501).json({ 'error': JSON.stringify(err) });
-			}
-		}
-	},
-	joinroom: {
-		post: function(req, res){
-			try{
-				let roomId = req.body.roomId;
-				// console.log(gameCode);
-				let gameCode = req.body.gameCode;
-				let password = req.body.password;
-				
-				if(gameCode === 0){
-					if(moleRooms[roomId].password === password || !moleRooms[roomId]){
-						moleRooms[roomId].userNum += 1;
-						res.status(200).json({'message': '방 입장에 성공했습니다'});
-					}else{
-						res.status(409).json({'error': '비밀번호가 틀렸습니다'});
-					}
-				}
-			} catch (err) {
-				console.log(err);
-				res.status(501).json({ 'error': JSON.stringify(err) });
-			}
-		}
-	},
-	leaveroom: {
-		post: function(req, res){
-			try{
-				let roomId = req.body.roomId;
-				let nickname = req.body.username;
-				let gameCode = req.body.gameCode;
-				
-				if(gameCode === 0){
-					// console.log(moleRooms[roomId]);
-					// console.log(nickname)
-					if(moleRooms[roomId].roomOwner === nickname){
-						delete moleRooms[roomId];
-						res.status(200).json({'message': '방장이 방을 끝냈습니다'});
-					} else {
-						moleRooms[roomId].userNum -=1;
-						if(moleRooms[roomId].userNum <=0) delete moleRooms[roomId];
-						res.status(200).json({'message': '한 명이 방을 나갔습니다'});
-					}
-				} else {
-					throw new Error('server Err');
-				}
-			} catch (err) {
-				console.log(err);
-				res.status(501).json({ 'error': JSON.stringify(err) });
-			}
-		}
-	},
+        if (gameCode === '1') {
+          getRooms = moleRooms;
+        } else if (gameCode === '2') {
+          getRooms = pongRooms;
+        } else if (gameCode === '3') {
+          getRooms = cardRooms;
+        }
+
+        Object.keys(getRooms).forEach((k) => {
+          let room = {};
+          room.roomId = getRooms[k].roomId;
+          room.gameCode = getRooms[k].gameCode;
+          room.roomName = getRooms[k].roomName;
+          room.roomOwner = getRooms[k].roomOwner;
+          room.userNum = getRooms[k].userNum;
+          room.isLocked = Boolean(getRooms[k].password);
+          rooms.push(room);
+        });
+        console.log(rooms);
+        // res.status(200).json(moleRooms);  //변경 필요
+        res.status(200).json(rooms);
+      } catch (err) {
+        console.log(err);
+        res.status(501).json({ error: JSON.stringify(err) });
+      }
+    },
+  },
+  joinroom: {
+    post: function (req, res) {
+      try {
+        let roomId = req.body.roomId;
+        let gameCode = req.body.gameCode;
+        let password = req.body.password;
+
+        if (gameCode === '1') {
+          if (moleRooms[roomId].password === password || !moleRooms[roomId]) {
+            if (moleRooms[roomId].userNum >= 2) {
+              return res.json({ error: '방이 꽉 찼습니다!' });
+            }
+            moleRooms[roomId].userNum += 1;
+            res.status(200).json({ message: '방 입장에 성공했습니다' });
+          } else {
+            res.status(409).json({ error: '비밀번호가 틀렸습니다' });
+          }
+        } else if (gameCode === '2') {
+          if (pongRooms[roomId].password === password || !pongRooms[roomId]) {
+            if (pongRooms[roomId].userNum >= 2) {
+              return res.json({ error: '방이 꽉 찼습니다!' });
+            }
+            pongRooms[roomId].userNum += 1;
+            res.status(200).json({ message: '방 입장에 성공했습니다' });
+          } else {
+            res.status(409).json({ error: '비밀번호가 틀렸습니다' });
+          }
+        } else if (gameCode === '3') {
+          if (cardRooms[roomId].password === password || !cardRooms[roomId]) {
+            if (cardRooms[roomId].userNum >= 2) {
+              return res.json({ error: '방이 꽉 찼습니다!' });
+            }
+            cardRooms[roomId].userNum += 1;
+            res.status(200).json({ message: '방 입장에 성공했습니다' });
+          } else {
+            res.status(409).json({ error: '비밀번호가 틀렸습니다' });
+          }
+        }
+      } catch (err) {
+        console.log(err);
+        res.status(501).json({ error: JSON.stringify(err) });
+      }
+    },
+  },
+  leaveroom: {
+    post: function (req, res) {
+      try {
+        let roomId = req.body.roomId;
+        let nickname = req.body.username;
+        let gameCode = req.body.gameCode;
+
+        if (gameCode === 0) {
+          // console.log(moleRooms[roomId]);
+          // console.log(nickname)
+          if (moleRooms[roomId].roomOwner === nickname) {
+            delete moleRooms[roomId];
+            res.status(200).json({ message: '방장이 방을 끝냈습니다' });
+          } else {
+            moleRooms[roomId].userNum -= 1;
+            if (moleRooms[roomId].userNum <= 0) delete moleRooms[roomId];
+            res.status(200).json({ message: '한 명이 방을 나갔습니다' });
+          }
+        } else {
+          throw new Error('server Err');
+        }
+      } catch (err) {
+        console.log(err);
+        res.status(501).json({ error: JSON.stringify(err) });
+      }
+    },
+  },
 };
--- web_server/db/config.js
+++ web_server/db/config.js
@@ -3,15 +3,14 @@ const mysql = require('mysql2');
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
+const development = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: process.env.DATABASE_DEV_PASSWORD,
+  database: '2p4p',
+});
 
-  const development = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: process.env.DATABASE_DEV_PASSWORD,
-    database: '2p4p',
-  });
-  
-  /* const production = mysql.createConnection({
+/* const production = mysql.createConnection({
     host: process.env.DATABASE_PRODUCTION_HOST,
     user: process.env.DATABASE_PRODUCTION_USER,
     password: process.env.DATABASE_PRODUCTION_PASSWORD,
@@ -19,6 +18,5 @@ require('dotenv').config({ path: path.join(__dirname, '../.env') });
   })
   poduction.connect()
    */
-  
-  module.exports = development;
 
+module.exports = development;
