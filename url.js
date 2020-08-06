fd3d792da1737bf1d7ed085a96df499a58a036f8
Wanderer-s <ows0871@gmail.com>
2020-08-06 14:45:26 +0900

    "url.js" remove '/'

--- url.js
+++ url.js
@@ -1,7 +1,7 @@
 let local = 'http://localhost'
-let web_server_url = `${local}:/3001`
-let game_mole_url = `${local}:/3009`
-let chat_server_url = `${local}:/3002`
+let web_server_url = `${local}:3001`
+let game_mole_url = `${local}:3009`
+let chat_server_url = `${local}:3002`
 
 module.exports = {
   web_server_url,
