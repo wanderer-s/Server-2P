a9418359e8da69015913c2a12078deb622ea605e
Wanderer-s <ows0871@gmail.com>
2020-08-07 11:11:15 +0900

    "db/config.js" change config for deploy version

--- web_server/db/config.js
+++ web_server/db/config.js
@@ -3,25 +3,18 @@ const mysql = require('mysql2');
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
-const development = mysql.createPool({
+/* const development = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: process.env.DATABASE_DEV_PASSWORD,
   database: '2p4p',
+}); */
+
+const production = mysql.createPool({
+  host: 5,
+  user: 5,
+  password: 5,
+  database: 'database2P'
 });
 
-module.exports = development;
-/* const production = mysql.createConnection({
-    host: process.env.DATABASE_PRODUCTION_HOST,
-    user: process.env.DATABASE_PRODUCTION_USER,
-    password: process.env.DATABASE_PRODUCTION_PASSWORD,
-    database: '2p4p'
-  })
-  poduction.connect()
-   */
-/* const production = mysql.createPool({
-    host: 'master.cxbrvfen2is1.ap-northeast-2.rds.amazonaws.com',
-    user: 'master',
-    password: 
-    database: 'database2P',
-  }); */
+module.exports = production;
