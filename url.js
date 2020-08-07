4122f39258f317dae606e357829f297ff0a9f89c
Wanderer-s <ows0871@gmail.com>
2020-08-07 11:10:41 +0900

    "url.js" change url for deploy urls

--- url.js
+++ url.js
@@ -1,11 +1,17 @@
-let local = 'http://localhost'
-let web_server_url = `${local}:3001`
-let game_mole_url = `${local}:3009`
-let chat_server_url = `${local}:3002`
+
+let web_server_url = 'http://3.34.178.78:3001'
+let bid_server_url = 'http://15.164.170.101:3005'
+let num_server_url = 'http://3.35.37.171:3006'
+let mole_server_url = 'http://13.125.22.16:3009'
+let chat_server_url = 'http://13.125.163.76:3002'
+let client = 'http://project-2p.s3-website.ap-northeast-2.amazonaws.com'
 
 
 module.exports = {
   web_server_url,
-  game_mole_url,
+  mole_server_url,
   chat_server_url,
+  bid_server_url,
+  num_server_url,
+  client
 };
