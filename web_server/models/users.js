1253a9b5abab21928d9f3045d5edbaf62f892dc6
Wanderer-s <ows0871@gmail.com>
2020-09-06 03:19:13 +0900

    refactor 'models/users/signin' promise callback to async await

--- web_server/models/users.js
+++ web_server/models/users.js
@@ -58,45 +58,54 @@ module.exports = {
 	signin: {
 		post: async function(data) {
 			let {userId, password, socialId, nickname} = await data;
-			// let shasum = crypto.pbkdf2Sync(password, '2p4P"sfinal_Project!', 942148, 64, 'sha512').toSting('hex');
-			// password = shasum;
-			if(!data.socialId) {	//소셜 로그인 아닌 경우
-				let key = crypto.pbkdf2Sync(password, '2p4P"sfinal_Project!', 942148, 64, 'sha512').toString('hex');
-				password = key;
-				return new Promise((resolve, reject) => {
-					let sql = `
+			let key = crypto.pbkdf2Sync(password, '2p4P"sfinal_Project!', 942148, 64, 'sha512').toString('hex');
+			password = key;
+			try {
+				if(!data.socialId) {
+					//socialId가 없을 시 즉 social login이 아닌 자체 login일 시
+					let loginSql = `
 							select
 								id,
 								nickname,
 								avatarId
 							from users
 							where userId = ? and password = ?`;
-					db.query(sql, [userId, password], (error, result) => {
-						if(!result.length) reject(new Error('loginErr'));
-						// console.log(result);
-						error ? reject(error) : resolve(result);
-					});
-				});
-			} else {
-				return new Promise((resolve, reject) => {
-					let sql = `
-							insert into users (userId, nickname, socialId)
+					
+					let [login] = (await asyncdb.query(loginSql, [userId, password]))[0];
+					if(!login) {
+						//loginSql 로 db 추출시에 값이 없는경우
+						//즉 회원가입을 하지 않았거나 Id 혹은 password가 틀린경우
+						throw 'loginErr'; //loginErr 로 throw
+					} return login; // controller로 login 전송 data는 중요하지 않고 이 함수가 끝나는게 중요
+				} else { //socialId가 있을 때 (social Login 시도할 때)
+					// 값이 있으면 아무 변화가 없고
+					// 값이 없으면 insert 하는 query 문
+					let insertSocialUser = `
+							insert into users
+							(userId, nickname, socialId)
 							select ?, ?, ?
 							from dual
 							where not exists(
 								select userId, socialId 
 								from users where socialId = ? and userId = ?)`;
-					db.query(sql, [userId, nickname, socialId, socialId, userId], (error) => {
-						if(error) {return reject(error);}
-						let sql = `
+					await asyncdb.query(insertSocialUser, [userId, nickname, socialId, socialId, userId]);
+					//db에 해당 userId와 socialId가 있으면 변화 X,
+					//없으면 해당 data 생성
+		
+					let loginSocialUser = `
 							select id, userId, nickname
 							from users
 							where userId = ? and socialId = ?`;
-						db.query(sql, [userId, socialId], (error, result) => {
-							error ? reject(error) : resolve(result);
-						}); 
-					});
-				});
+					let [socialLogin] = (await asyncdb.query(loginSocialUser, [userId, socialId]))[0];
+					if(!socialLogin) {
+						//userId와 socialId로 select
+						//추출이 안되면 loginErr throw(socialId라 그런경우가 없겠지만 만일에 대비하여)
+						throw 'loginErr';
+					}
+					return socialLogin;
+				}
+			} catch(error) {
+				throw Error(error);
 			}
 		}
 	}
